eefd15040a1343eb61608662533e949a8c5f3774
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2025-12-16 15:33:30 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  // This tells Next.js that your site lives in a subfolder
+  basePath: "/JiyathPortfolio",
+
+  // This forces the CSS/JS to load from that subfolder (Critical for GitHub Pages)
+  assetPrefix: "/JiyathPortfolio/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
